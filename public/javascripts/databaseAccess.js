831ee39c9bba05240dd7ab2f4ad17bac939f79ea
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-11-14 13:05:24 +0000

    mysql start

---
+++ public/javascripts/databaseAccess.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost:3000',
+  user: 'libUser',
+  password: 's3kreee7',
+  database: 'my_db'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
