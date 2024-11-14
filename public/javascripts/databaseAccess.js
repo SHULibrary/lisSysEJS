dd32080088fb6edb769b31184c916f1d5be9d962
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-11-14 15:33:41 +0000

    work

--- public/javascripts/databaseAccess.js
+++ public/javascripts/databaseAccess.js
@@ -1,17 +1,7 @@
-var mysql = require('mysql')
-const connection = mysql.createConnection({
-  host: 'localhost:3000',
-  user: 'libUser',
-  password: 's3kreee7',
-  database: 'my_db'
-})
+let button = document.getElementById("runDB");
+button.addEventListener("click", () => runCommand());
 
-connection.connect()
 
-connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
-})
-
-connection.end()
+function runCommand() {
+  console.log("one");
+}
