9b784b55b244d2334b2b1dad05eec8ab5261ea1e
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-11-15 15:32:40 +0000

    sqlWork

--- public/javascripts/databaseAccess.js
+++ public/javascripts/databaseAccess.js
@@ -1,12 +1,7 @@
-
 let dbButton = document.querySelector('#submit-button');
+
 dbButton.onclick = () => {
-  const db = new sqlite3.Database(path.join(__dirname, 'data', 'libData.db'), (err) => {
-  if (err) {
-    console.error('Error opening database', err);
-  } else {
-    console.log('Connected to SQLite database');
-  }
+  var username = document.getElementById('username-box');
+  var password = document.getElementById('password-box');
   
-});
 }
