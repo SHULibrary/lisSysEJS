e75959ed3172d73924eb72f1497e8e58e3b93a04
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-11-15 14:21:44 +0000

    sql progress

--- public/javascripts/databaseAccess.js
+++ public/javascripts/databaseAccess.js
@@ -1,6 +1,14 @@
-let button = document.getElementById("runDB");
-button.addEventListener("click", () => runCommand());
 
-function runCommand() {
-  button.textContent = "ran";
+let dbButton = document.querySelector('#runDB');
+dbButton.onclick = runCode;
+
+function runCode(){
+  const db = new sqlite3.Database(path.join(__dirname, 'data', 'libData.db'), (err) => {
+  if (err) {
+    console.error('Error opening database', err);
+  } else {
+    console.log('Connected to SQLite database');
+  }
+  
+});
 }
