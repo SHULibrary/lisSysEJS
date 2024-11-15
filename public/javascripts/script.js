6372576608d4188761f16e62b6a530c3c2bf5116
QistinaRoslan <c2039618@hallam.shu.ac.uk>
2024-11-15 15:32:16 +0000

    More Info

---
+++ public/javascripts/script.js
@@ -0,0 +1,9 @@
+const submit_button = document.getElementById('sumbit-button')
+
+submit_button.addEventListener("click", () => {
+    alert ('Form is submitted!')
+})
+
+function submit_function () {
+    alert ('The submit function works perfectly!')
+}
