cd9b3b53ca94a3cad428cdbb95495c8b47ec74fe
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-11-15 14:30:14 +0000

    adfs

--- public/javascripts/databaseAccess.js
+++ public/javascripts/databaseAccess.js
@@ -1,8 +1,6 @@
 
-let dbButton = document.querySelector('#runDB');
-dbButton.onclick = runCode;
-
-function runCode(){
+let dbButton = document.querySelector('#submit-button');
+dbButton.onclick = () => {
   const db = new sqlite3.Database(path.join(__dirname, 'data', 'libData.db'), (err) => {
   if (err) {
     console.error('Error opening database', err);
