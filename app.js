e535c4ab9c7e315eee3e0f5a693f521c519ca315
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-11-15 14:25:35 +0000

    chanegs

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get('/api/users', (req, res) => {
   const { login, password } = req.query;
   let query = "SELECT * FROM users WHERE (email = " + login + " AND password = " + password + ") OR (username = " + login + " AND password = " + password + ")";
-
   db.all(query, (err, rows) => {
     if (err) {
       console.error('Error fetching users', err);
