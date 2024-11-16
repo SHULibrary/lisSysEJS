7ec47bfe159b05818c78e47b14574b5f320c3f1d
aslynerithika <aslynerithika2003@gmail.com>
2024-11-16 15:38:17 +0000

    feat: hompage recommendations and categories

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 router.get("/", function (req, res, next) {
   const recommendations = [
-    {
+    /*{
       name: "The Psychology Of Money",
       author: "Morgan Housel",
       image: "/images/books/the-psychology-of-money.jpg",
-    },
+    },*/
     {
       name: "How Innovation Works",
       author: "Matt Ridley",
@@ -21,7 +21,27 @@ router.get("/", function (req, res, next) {
     {
       name: "Iron Flame",
       author: "Rebecca Yarros",
-      image: "/images/books/iron-flame.webp",
+      image: "/images/books/iron-flame.png",
+    },
+    {
+      name: "The Lost Bookshop",
+      author: "Evie Woods",
+      image: "/images/books/the-lost-bookshop.jpg",
+    },
+    {
+      name: "Onyx Storm",
+      author: "Rebecca Yarros",
+      image: "/images/books/onyx-storm.jpg",
+    },
+    {
+      name: "A Kingdom of Flesh and Fire",
+      author: "Jennifer L. Armentrout",
+      image: "/images/books/a-kingdom-of-flesh-and-fire.jpg",
+    },
+    {
+      name: "Phantasma: A dark fantasy romance",
+      author: "Kaylie Smith",
+      image: "/images/books/phantasma-a-dark-fantasy-romance.jpg",
     },
   ];
 
