9f9af157eb26c4e1b6ff198d4df363d1f3c60d12
QistinaRoslan <c2039618@hallam.shu.ac.uk>
2024-11-17 15:19:45 +0000

    gdh

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,24 @@
-const submit_button = document.getElementById('sumbit-button')
+const submit_button = document.getElementById('sumbit-button').addEventListener('click', function () {
+    // Get input values
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    // Find matching user in database
+    
+    const user = mockUsers.find(
+        u => u.username === username && u.password === password
+      );  
 
-submit_button.addEventListener("click", () => {
-    alert ('Form is submitted!')
-})
-
-function submit_function () {
-    alert ('The submit function works perfectly!')
-}
+      //Display feedback to user
+      const messageElement = document.getElementById('message');
+      if (user) {
+        messageElement.textContent = 'Login successful!';
+        messageElement.style.color = 'green';
+        // Redirect or perform further actions
+      } else {
+        messageElement.textContent = 'Invalid username or password.';
+        messageElement.style.color = 'red';
+      }
+    });
+async function getUsers() {
+    return await getItems("users");
+  }
