585a250c6392cfc838646f292d08ef3d04a6c01c
QistinaRoslan <c2039618@hallam.shu.ac.uk>
2024-11-17 15:46:33 +0000

    login

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,24 +1,23 @@
-const submit_button = document.getElementById('sumbit-button').addEventListener('click', function () {
-    // Get input values
-    const username = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value.trim();
-    // Find matching user in database
-    
-    const user = mockUsers.find(
-        u => u.username === username && u.password === password
-      );  
+const { getItems } = require("../../server");
 
-      //Display feedback to user
-      const messageElement = document.getElementById('message');
-      if (user) {
-        messageElement.textContent = 'Login successful!';
-        messageElement.style.color = 'green';
-        // Redirect or perform further actions
-      } else {
+const submit_button = document.getElementById('sumbit-button').addEventListener('click', async function () {
+    // Get input values
+    var users = await getItems("users");
+    const username = document.getElementById('username-box').value.trim();
+    const password = document.getElementById('password-box').value.trim();
+    var currentUser = null;
+    users.forEach(u => {
+        if((u.username == username || u.email == username) && u.password == password){
+            currentUser = u;
+            throw {};           
+        }
+    });
+    if (currentUser != null) {
+        //route to index
+        window.location.href = '/index';
+    }
+    else {
         messageElement.textContent = 'Invalid username or password.';
         messageElement.style.color = 'red';
       }
     });
-async function getUsers() {
-    return await getItems("users");
-  }
