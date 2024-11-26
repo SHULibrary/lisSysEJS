333381ec985c29e73ce17eea262e1e3859bcc3c9
aslynerithika <aslynerithika2003@gmail.com>
2024-11-26 14:08:07 +0000

    wishlistpage

--- server.js
+++ server.js
@@ -33,12 +33,23 @@ async function getItems(table) {
   }
 }
 
-async function getBooks() {
-  const query = "SELECT * FROM book INNER JOIN media ON book.id = media.id";
+async function getBooks(userId) {
+  const query = `
+    SELECT 
+      book.*,
+      media.*,
+      CASE 
+        WHEN wishlist.mediaID IS NOT NULL THEN 1
+        ELSE 0
+      END as wishlisted
+    FROM book 
+    INNER JOIN media ON book.id = media.id
+    LEFT JOIN wishlist ON media.id = wishlist.mediaID AND wishlist.userID = ?
+  `;
 
   try {
     return new Promise((resolve, reject) => {
-      db.all(query, [], (err, rows) => {
+      db.all(query, [userId], (err, rows) => {
         if (err) {
           reject(err);
         } else {
@@ -52,4 +63,57 @@ async function getBooks() {
   }
 }
 
-module.exports = { getItems, getBooks };
+async function getWishlist(userId) {
+  const query =
+    "SELECT * FROM wishlist INNER JOIN media ON wishlist.mediaID = media.id INNER JOIN book ON media.id = book.id WHERE wishlist.userID = ?";
+
+  try {
+    return new Promise((resolve, reject) => {
+      db.all(query, [userId], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  } catch (error) {
+    console.error("Error fetching :" + table, error.message);
+    throw error;
+  }
+}
+
+async function isWishlisted(userId, mediaId) {
+  const query = "SELECT * FROM wishlist WHERE userID = ? AND mediaID = ?";
+
+  try {
+    const rows = new Promise((resolve, reject) => {
+      db.all(query, [userId, mediaId], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+
+    return (await rows).length;
+  } catch (error) {
+    console.error("Error fetching :" + table, error.message);
+    throw error;
+  }
+}
+
+async function wishlistMedia(userId, mediaId) {
+  let query = "INSERT INTO wishlist (mediaID, userID) VALUES (?, ?)";
+
+  let exists = await isWishlisted(userId, mediaId);
+  if (exists) {
+    query = "DELETE FROM wishlist WHERE mediaID = ? AND userID = ?";
+  }
+
+  db.run(query, [mediaId, userId]);
+  return !exists;
+}
+
+module.exports = { getItems, getBooks, getWishlist, wishlistMedia };
