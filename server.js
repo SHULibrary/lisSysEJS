d22b3d5485c2cd2433e2e09f627e5008a4f03769
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-11-30 14:43:00 +0000

    SERVER STUFF

--- server.js
+++ server.js
@@ -1,27 +1,67 @@
 var express = require("express");
 var sqlite3 = require("sqlite3").verbose();
+const mysql = require('mysql');
 
 var app = express();
 
-const db = new sqlite3.Database("./data/libData.db", (err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err.message);
-  } else {
-    console.log("Connected to SQLite database.");
-  }
+var checkedOutData;
+var mediaData;
+var reservedData;
+var usersData;
+var wishlistData;
+
+
+
+// conn.query('SELECT * FROM `checkedout`', function (error, results, fields) {
+//   checkedOutData = results;  
+// });
+
+// conn.query('SELECT * FROM `media`', function (error, results, fields) {
+//   mediaData = results;
+// });
+
+// conn.query('SELECT * FROM `reservations`', function (error, results, fields) {
+//   reservedData = results;  
+// });
+
+// conn.query('SELECT * FROM `users`', function (error, results, fields) {
+//   usersData = results;  
+// });
+
+// conn.query('SELECT * FROM `wishlist`', function (error, results, fields) {
+//   wishlistData = results;  
+// });
+
+// console.log(checkedOutData);
+// console.log(mediaData);
+// console.log(reservedData);
+// console.log(usersData);
+// console.log(wishlistData);
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'libdb',
+  port: '3306'
+});
+
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
 });
 
 async function getItems(table) {
   const query = "SELECT * FROM " + table;
   try {
     return new Promise((resolve, reject) => {
-      db.all(query, [], (err, rows) => {
-        if (err) {
-          reject(err);
+      conn.query(query, function (error, results, fields) {
+        if (error) {
+          reject(error);
         } else {
-          resolve(rows);
+          resolve(results);
         }
-      });
+      })
     });
   } catch (error) {
     console.error("Error fetching :" + table, error.message);
@@ -32,26 +72,24 @@ async function getItems(table) {
 async function getBooks(userId) {
   const query = `
     SELECT 
-      book.*,
       media.*,
       CASE 
-        WHEN wishlist.mediaID IS NOT NULL THEN 1
+        WHEN wishlist.media_id IS NOT NULL THEN 1
         ELSE 0
       END as wishlisted
-    FROM book 
-    INNER JOIN media ON book.id = media.id
-    LEFT JOIN wishlist ON media.id = wishlist.mediaID AND wishlist.userID = ?
+    FROM media
+    LEFT JOIN wishlist ON media.id = wishlist.media_id AND wishlist.user_id = ?
   `;
 
   try {
     return new Promise((resolve, reject) => {
-      db.all(query, [userId], (err, rows) => {
-        if (err) {
-          reject(err);
+      conn.query(query, [userId], function (error, results, fields) {
+        if (error) {
+          reject(error);
         } else {
-          resolve(rows);
+          resolve(results);
         }
-      });
+      })
     });
   } catch (error) {
     console.error("Error fetching :" + table, error.message);
@@ -61,39 +99,35 @@ async function getBooks(userId) {
 
 async function getWishlist(userId) {
   const query =
-    "SELECT * FROM wishlist INNER JOIN media ON wishlist.mediaID = media.id INNER JOIN book ON media.id = book.id WHERE wishlist.userID = ?";
-
-  try {
-    return new Promise((resolve, reject) => {
-      db.all(query, [userId], (err, rows) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(rows);
-        }
+    "SELECT * FROM wishlist INNER JOIN media ON wishlist.media_id = media.id";
+    try {
+      return new Promise((resolve, reject) => {
+        conn.query(query, function (error, results, fields) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
+        })
       });
-    });
-  } catch (error) {
-    console.error("Error fetching :" + table, error.message);
-    throw error;
-  }
+    } catch (error) {
+      console.error("Error fetching :" + table, error.message);
+      throw error;
+    }
 }
 
 async function isWishlisted(userId, mediaId) {
-  const query = "SELECT * FROM wishlist WHERE userID = ? AND mediaID = ?";
-
+  const query = "SELECT * FROM wishlist WHERE user_id = ? AND media_id = ?";
   try {
-    const rows = new Promise((resolve, reject) => {
-      db.all(query, [userId, mediaId], (err, rows) => {
-        if (err) {
-          reject(err);
+    return new Promise((resolve, reject) => {
+      conn.query(query, [userId, mediaId],function (error, results, fields) {
+        if (error) {
+          reject(error);
         } else {
-          resolve(rows);
+          resolve(results);
         }
-      });
+      })
     });
-
-    return (await rows).length;
   } catch (error) {
     console.error("Error fetching :" + table, error.message);
     throw error;
@@ -101,19 +135,15 @@ async function isWishlisted(userId, mediaId) {
 }
 
 async function wishlistMedia(userId, mediaId) {
-  let query = "INSERT INTO wishlist (mediaID, userID) VALUES (?, ?)";
+  let query = "INSERT INTO wishlist (media_id, user_id) VALUES (?, ?)";
 
   let exists = await isWishlisted(userId, mediaId);
   if (exists) {
-    query = "DELETE FROM wishlist WHERE mediaID = ? AND userID = ?";
+    query = "DELETE FROM wishlist WHERE media_id = ? AND user_id = ?";
   }
 
-  db.run(query, [mediaId, userId]);
+  conn.run(query, [mediaId, userId]);
   return !exists;
 }
 
-function test() {
-  console.log("hi here");
-}
-
-module.exports = { getItems, getBooks, getWishlist, wishlistMedia, test };
+module.exports = { getItems, getBooks, getWishlist, wishlistMedia };
