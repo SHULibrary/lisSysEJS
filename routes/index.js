3d676eccb66181b056ab41876be2c4ea7b51dcc8
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-12-03 12:25:45 +0000

    changes

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,14 @@ var { getBooks } = require("../server")
 const USER_ID = 3;
 
 router.get("/", async function (req, res, next) {
-  const books = await getBooks(USER_ID);
+  var books = await getBooks(USER_ID);
   // TODO: instead of slicing maybe get most wishlisted book
-  // or by reservations amount idk?
   const recommendations = books.slice(0, 6);
-
+  books.forEach(book => {
+    if(book.image == "") {
+      book.image = "/images/books/placeholder.png";
+    }
+  });
   res.render("index", {
     books,
     recommendations,
--- routes/media.js
+++ routes/media.js
@@ -6,9 +6,10 @@ var router = express.Router();
 
 router.get('/', async function(req, res, next) {
     const { id } = req.query;
-    const book = await getBook(id, 3);
+    var book = await getBook(id, 3);
     if (book.length > 0) {
-        res.render('media', { book: book[0] });
+      //console.log(book.image);
+      res.render('media', { book: book[0] });
     }
   });
 
