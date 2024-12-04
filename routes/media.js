142311acaff4910837cf7d3d0f99e0f9365c92a1
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-12-04 15:21:03 +0000

    edit funmction nearly

--- routes/media.js
+++ routes/media.js
@@ -5,22 +5,25 @@ var router = express.Router();
 /* GET media page. */
 
 router.get('/', async function(req, res, next) {
-    const { id } = req.query;
-    var book = await getBook(id, 3);
-    if (book.image == "") book.image = "images/books/placeholder.png";
-    if (book.length > 0) {
-      res.render('media', { book: book[0] });
-    }
-  });
+  const { id } = req.query;
+  var book = await getBook(id, 3);
+  if (book.image == "") book.image = "images/books/placeholder.png";
+  if (book.length > 0) {
+    res.render('media', { book: book[0] });
+  }
+});
 
-  router.post("/", async function (req, res, next) {
-    const { name, description, author, mediaID } = req.body;
-    try {
-      const success = await editMedia(name, description, author, numAvail, numOf, mediaID);
-      res.send({ success });
-    } catch {
-      res.send({ success: false });
-    }
-  });
+router.post("/", async function (req, res, next) {
+  console.log("HELLO")
+  const { name, description, author, numAvail, numOf, mediaID } = req.body;
+  try {
+    const success = await editMedia(name, description, author, numAvail, numOf, mediaID);
+    console.log(success);
+    res.send({ success });
+  } catch {
+    res.send({ success: false });
+    console.log("FAIL");
+  }
+});
 
   module.exports = router;
