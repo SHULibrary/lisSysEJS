fd0d609cab65dd9c709ddab48900f792477f583b
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-12-06 12:09:16 +0000

    sadfc

--- routes/add-media.js
+++ routes/add-media.js
@@ -4,16 +4,13 @@ var router = express.Router();
 
 
 router.get('/', async function(req, res, next) {
-    console.log("HELLO");
     res.render('add-media');
 });
 
 router.post("/", async function (req, res, next) {
-  console.log("HELLO2");
   const { name, description, author, numAvail, numOf } = req.body;
   try {
     const success = await addMedia(name, description, author, numAvail, numOf);
-    console.log("HELLOadfklj;")
     res.send({ success });
   } catch {
     res.send({ success: false });
