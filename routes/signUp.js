48b443c4ccc38c4b40d874230230ad9fdf682749
aslynerithika <aslynerithika2003@gmail.com>
2024-12-07 16:54:19 +0000

    fix: remove bycrypt import

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,19 +1,18 @@
-import bcrypt from bcrypt
-var express = require('express');
-const { createUser } = require('../server');
+var express = require("express");
+const { createUser } = require("../server");
 var router = express.Router();
 
 /* GET sign in page. */
 
-router.get('/', function(req, res, next) {
-  res.render('signUp', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("signUp", { title: "Express" });
 });
 
-router.post("/", async function(req, res, next) {
-  const {name, email, username, password, DOB, number} = req.body;
+router.post("/", async function (req, res, next) {
+  const { name, email, username, password, DOB, number } = req.body;
   if (createUser(name, email, username, password, DOB, number)) {
     res.redirect(301, "/");
   }
-})
+});
 
 module.exports = router;
