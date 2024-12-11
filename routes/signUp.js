40e3249533a27c35afc09c8fffa85c8ff42e4777
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-12-11 13:22:07 +0000

    sdv

--- routes/signUp.js
+++ routes/signUp.js
@@ -17,22 +17,6 @@ router.get("/", function (req, res, next) {
 
 router.post("/", async function(req, res, next) {
   const {name, email, username, password, DOB, number} = req.body;
-  bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
-    if(err) return res.json({Error: "Error for hashing password"});
-    const values = [
-      req.body.name,
-      req.body.email,
-      req.body.username,
-      hash,
-      req.body.DOB,
-      req.body.number
-    ]
-    db.query(sql, [values], (err, reslut) => {
-      if(err) return res.json({Error: "Inserting data Error in server"});
-      return res.json({Status: "Success"});
-    }
-    )
-  })
   if (createUser(name, email, username, password, DOB, number)) {
     res.redirect(301, "/");
   }
