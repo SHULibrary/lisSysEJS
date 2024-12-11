aabfc50a7ccb057eea90659e88848f474e7dd589
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-12-11 13:23:43 +0000

    g

--- routes/media.js
+++ routes/media.js
@@ -13,7 +13,7 @@ router.get('/', async function(req, res, next) {
     var book = await getBook(id, 3);
     if (book.image == "") book.image = "images/books/placeholder.png";
     if (book.length > 0) {
-      res.render('media', { book: book[0] });
+      res.render('media', { book: book[0], user: req.session.user });
     }
   }
   
