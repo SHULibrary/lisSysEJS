96ab858fa775d6d20835c38f206bc5938f154840
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-12-11 13:06:52 +0000

    wishlist query fix

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ async function getBooks(userId, search) {
 
 async function getList(userId, list) {
   const query =
-    "SELECT * FROM " + list + " INNER JOIN media ON " + list + ".media_id = media.id";
+    "SELECT * FROM " + list + " INNER JOIN media ON " + list + ".media_id = media.id AND " + list + ".user_id = " + userId;
     try {
       return new Promise((resolve, reject) => {
         conn.query(query, function (error, results, fields) {
