14348d8b7a131408011d16b9d1adf856619742e5
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-12-16 17:20:06 +0000

    asc

--- test.test.js
+++ test.test.js
@@ -2,122 +2,57 @@
 const express = require("express")
 const routes = require("./routes")
 const mysql = require('mysql');
-const { getList, createUser } = require('./server');
+const { getList, createUser, getSingle } = require('./server');
 
 var app = express();
 
-
-
-// const conn = mysql.createConnection({
-//             host: 'localhost',
-//             user: 'root',
-//             password: '',
-//             database: 'libdb',
-//             port: '3306'
-//           });
+const conn = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'libdb',
+            port: '3306'
+          });
     
-//           conn.connect((err) =>{
-//             if(err) throw err;
-//           });
+          conn.connect((err) =>{
+            if(err) throw err;
+          });
 
-// async function getList(userId, list) {
-//   const query =
-//     "SELECT * FROM " + list + " INNER JOIN media ON " + list + ".media_id = media.id AND " + list + ".user_id = " + userId;
-//     try {
-//       return new Promise((resolve, reject) => {
-//         conn.query(query, function (error, results, fields) {
-//           if (error) {
-//             reject(error);
-//           } else {
-//             resolve(results);
-//           }
-//         })
-//       });
-//     } catch (error) {
-//       console.error("Error fetching :" + table, error.message);
-//       throw error;
-//     }
-// }
-// async function deleteBook() {
-//   if (confirm("Are you sure you want to delete <%= book.name %>") == true) {
-//     const request = await fetch("/search", {
-//       method: "POST",
-//       body: JSON.stringify({
-//         mediaId: <%= book.id %>,
-//       }),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     });
-//     window.location.href = "/";
-//     return request;
-//   }
-// }
-
-// test('fetch wishlist', () => {
-//     expect(getList(3, "wishlist")).toBe();
-//   });
-
-// const conn = mysql.createConnection({
-//             host: 'localhost',
-//             user: 'root',
-//             password: '',
-//             database: 'libdb',
-//             port: '3306'
-//           });
+test('test db connection (SUCCESS)', () => {
+    const conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'libdb',
+        port: '3306'
+      });
     
-//           conn.connect((err) =>{
-//             if(err) throw err;
-//           });
+      var expItem = 1;
+
+      conn.connect((err) =>{
+        if(err) expItem = 0; //since the connection cannot be tested as a variable, I made the expected variable change if the DB does not connect
+      });
+      expect(expItem).toBe(1);
+});
 
 
-// async function getSingle(id) {
-//   const query =
-//     "SELECT * FROM media WHERE id = ?";
-//     try {
-//       return new Promise((resolve, reject) => {
-//         conn.query(query, [id], function (error, results, fields) {
-//           if (error) {
-//             reject(error);
-//           } else {
-//             resolve(results);
-//           }
-//         })
-//       });
-//     } catch (error) {
-//       console.error("Error fetching :" + table, error.message);
-//       throw error;
-//     }
-// }
-
-// test('test db connection (SUCCESS)', () => {
-//     const conn = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: '',
-//         database: 'libdb',
-//         port: '3306'
-//       });
-    
-//       var expItem = 1;
-
-//       conn.connect((err) =>{
-//         if(err) expItem = 0; //since the connection cannot be tested as a variable, I made the expected variable change if the DB does not connect
-//       });
-//       expect(expItem).toBe(1);
-// });
 
 test('fetch reservations unresolved', async () => {
     var reservations = await getList(3, "reservations");
     reservations[0]["dateEntered"] = '2024-11-16T00:00:00.000Z'
-    expect(reservations[0]).toBe({"author": "Matt Ridley", "dateEntered": '2024-11-16T00:00:00.000Z', "description": "When government analyst Kate Halloran stumbles upon a classified file that wasn’t meant for her eyes, she’s pulled into a conspiracy that spans continents.", "id": 1, "image": "/images/books/how-innovation-works.jpg"});
+    expect(reservations[0]).toEqual({"author": "Matt Ridley", "dateEntered": '2024-11-16T00:00:00.000Z', "description": "When government analyst Kate Halloran stumbles upon a classified file that wasn’t meant for her eyes, she’s pulled into a conspiracy that spans continents.", "id": 1, "image": "/images/books/how-innovation-works.jpg", "media_id": 1, "name": "How Innovation Works", "numberAvail": 4, "numberOf": 4, "user_id": 3});
 });
+
+test('fetch media availabiliy', async () => {
+    var item = await getSingle(1);
+    expect(item[0]["numberAvail"]).toEqual(4);
+});
+
 
     test('should return error if any field is missing', async () => {
       expect(await createUser('', 'email@email.com', 'Username', 'Password1', 'dd/mm/yyyy', 102012)).toEqual("All fields are required.")
       });
-  // test('create user function correctly', async () => {
-  //   var create = await createUser('Full Name', 'email@email.com', 'Username', 'Password1', '01/01/1000', 871387);
-  //   //CHANGE 'insertId' TO CURRENT NUMBER + 1 EVERY TIME THIS TEST IS RAN
-  //   expect(create).toEqual({"affectedRows": 1, "changedRows": 0, "fieldCount": 0, "insertId": 13, "message": "", "protocol41": true, "serverStatus": 2, "warningCount": 3});
-  // });
+  test('create user function correctly', async () => {
+    var create = await createUser('Full Name', 'email@email.com', 'Username', 'Password1', '01/01/1000', 871387);
+    expect(create["affectedRows"]).toEqual(1);
+  });
