2f08a8f089db05dbcb0d2fdec76e4360f1f549fa
aslynerithika <aslynerithika2003@gmail.com>
2024-12-16 14:38:56 +0000

    added unit test

--- test.test.js
+++ test.test.js
@@ -1,42 +1,20 @@
-const express = require("express")
-const routes = require("./routes")
-const mysql = require('mysql');
-const { resolve } = require("path");
+const { getList, ListMedia, getBooks } = require("./server");
 
-var app = express();
+const USER_ID = 3;
+const MEDIA_ID = 3;
 
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'libdb',
-  port: '3306'
+test("fetch wishlist", async () => {
+  const wishlist = await getList(USER_ID, "wishlist");
+  expect(wishlist).toEqual(expect.any(Array));
 });
 
-conn.connect((err) =>{
-  if(err) throw err;
-  //console.log('Mysql Connected...');
+test("wishlist media", async () => {
+  const success = await ListMedia("wishlist", USER_ID, MEDIA_ID);
+  const success2 = await ListMedia("wishlist", USER_ID, MEDIA_ID);
+  expect(success2).toBe(!success);
 });
 
-async function getList(userId, list) {
-  const query =
-    "SELECT * FROM " + list + " INNER JOIN media ON " + list + ".media_id = media.id AND " + list + ".user_id = " + userId;
-    try {
-      return new Promise((resolve, reject) => {
-        conn.query(query, function (error, results, fields) {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(results);
-          }
-        })
-      });
-    } catch (error) {
-      console.error("Error fetching :" + table, error.message);
-      throw error;
-    }
-}
-
-test('fetch wishlist', () => {
-    expect(getList(3, "wishlist")).toBe();
-  });
+test("search media", async () => {
+  const books = await getBooks(USER_ID, "Iron Flame");
+  expect(books.length).toBe(1);
+});
