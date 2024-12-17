ed17cd68ac73b494766e1bc90f14a84ac71d40eb
c2077151 <117455975+c2077151@users.noreply.github.com>
2024-12-17 17:52:04 +0000

    C

--- test.test.js
+++ test.test.js
@@ -4,6 +4,7 @@ const {
   getBooks,
   addMedia,
   createUser,
+  getSingle,
 } = require("./server");
 const mysql = require("mysql");
 
@@ -43,57 +44,60 @@ test("test db connection (SUCCESS)", () => {
   expect(expItem).toBe(1);
 });
 
-// test('fetch reservations unresolved', async () => {
-//     var reservations = await getList(3, "reservations");
-//     reservations[0]["dateEntered"] = '2024-11-16T00:00:00.000Z'
-//     expect(reservations[0]).toEqual({"author": "Matt Ridley", "dateEntered": '2024-11-16T00:00:00.000Z', "description": "When government analyst Kate Halloran stumbles upon a classified file that wasn’t meant for her eyes, she’s pulled into a conspiracy that spans continents.", "id": 1, "image": "/images/books/how-innovation-works.jpg", "media_id": 1, "name": "How Innovation Works", "numberAvail": 4, "numberOf": 4, "user_id": 3});
-// });
+test('fetch reservations', async () => {
+    var reservations = await getList(3, "reservations");
+    expect(reservations[0]["author"]).toEqual("Matt Ridley");
+});
 
-// test('fetch media availabiliy', async () => {
-//     var item = await getSingle(1);
-//     expect(item[0]["numberAvail"]).toEqual(4);
-// });
+test('fetch media availabiliy', async () => {
+    var item = await getSingle(1);
+    expect(item[0]["numberAvail"]).toEqual(4);
+});
+
+
+//THE BELOW TESTS WORK HOWEVER ARE TABBED OUT UNTIL WANTED SINCE THEY CREATE DATA IN THE DB
+
 
 //TEST SIGN UP
-test("should return error if any field is missing", async () => {
-  expect(
-    await createUser(
-      "",
-      "email@email.com",
-      "Username",
-      "Password1",
-      "dd/mm/yyyy",
-      102012
-    )
-  ).toEqual("All fields are required.");
-});
-test("create user function correctly", async () => {
-  var create = await createUser(
-    "Full Name",
-    "email@email.com",
-    "Username",
-    "Password1",
-    "01/01/1000",
-    871387
-  );
-  expect(create["affectedRows"]).toEqual(1);
-});
+// test("should return error if any field is missing", async () => {
+//   expect(
+//     await createUser(
+//       "",
+//       "email@email.com",
+//       "Username",
+//       "Password1",
+//       "dd/mm/yyyy",
+//       102012
+//     )
+//   ).toEqual("All fields are required.");
+// });
+// test("create user function correctly", async () => {
+//   var create = await createUser(
+//     "Full Name",
+//     "email@email.com",
+//     "Username",
+//     "Password1",
+//     "01/01/1000",
+//     871387
+//   );
+//   expect(create["affectedRows"]).toEqual(1);
+// });
 //TEST ADD MEDIA
-test("should return error if any field is missing", async () => {
-  expect(
-    await addMedia("", "description", "author", "numAvail", "numOf")
-  ).toEqual("All fields are required.");
-});
-test("create user function correctly", async () => {
-  var create = await addMedia(
-    "name",
-    "description",
-    "author",
-    "numAvail",
-    "numOf"
-  );
-  expect(create["affectedRows"]).toEqual(1);
-});
+// test("should return error if any field is missing", async () => {
+//   expect(
+//     await addMedia("", "description", "author", "numAvail", "numOf")
+//   ).toEqual("All fields are required.");
+// });
+// test("create user function correctly", async () => {
+//   var create = await addMedia(
+//     "name",
+//     "description",
+//     "author",
+//     "numAvail",
+//     "numOf"
+//   );
+//   expect(create["affectedRows"]).toEqual(1);
+// });
 // test('create user function correctly', async () => {
 //   var create = await createUser('Full Name', 'email@email.com', 'Username', 'Password1', '01/01/1000', 871387);
 //   expect(create).toEqual({"affectedRows": 1, "changedRows": 0, "fieldCount": 0, "insertId": 13, "message": "", "protocol41": true, "serverStatus": 2, "warningCount": 3});
